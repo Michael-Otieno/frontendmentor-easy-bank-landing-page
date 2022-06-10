7550ecf86af980e06d78c0a4a744a7c12b78bca6
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-11 02:21:13 +0300

    add home compnent

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,50 @@
-export default function Navbar(){
+import hamburgerIcon from "../../images/icon-hamburger.svg";
+import closeIcon from "../../images/icon-close.svg";
+import logo from "../../images/logo.svg";
+import "./Navbar.css";
+
+import { useState } from "react";
+
+export default function Navbar() {
+  const [navExpanded, setNavExpand] = useState(false);
+
+  function toggleNav() {
+    setNavExpand((prevState) => !prevState);
+  }
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <nav className="nav-menu">
+      <a className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+      <button className="hamburger" onClick={toggleNav}>
+        <img
+          src={navExpanded ? closeIcon : hamburgerIcon}
+          alt="hamburger icon"
+        />
+      </button>
+      <div className={navExpanded ? "nav-links " : "nav-links expand-menu"}>
+        <ul className="nav-list">
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/contact">Blog</a>
+          </li>
+          <li>
+            <a href="/contact">Careers</a>
+          </li>
+        </ul>
+        <button 
+        className="nav-request-btn"
+        >Request Invite
+        </button>
+      </div>
+    </nav>
+  );
 }
