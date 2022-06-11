68cbf8c6517a5e8f78685b51c683af5f02e60a1a
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-06-12 00:03:40 +0300

    add home page details and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 import './App.css';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 }
